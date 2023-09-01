8d7533dc0d1f8a437deed651c4f55170a6e7e18f
Qiyuan-Ge <53368178+Qiyuan-Ge@users.noreply.github.com>
2023-09-01 11:54:23 +0800

    Update llm_math.py

--- open_assistant/tools/llm_math.py
+++ open_assistant/tools/llm_math.py
@@ -1,20 +1,23 @@
 import re
+import math
 import openai
 import numexpr
 
 prompt_template = """Translate a math problem into a expression that can be executed using Python's numexpr library. Use the output of running this code to answer the question.
 
+Using the following format:
+
 Question: ${{Question with math problem.}}
 ```text
 ${{single line mathematical expression that solves the problem}}
 ```
-...numexpr.evaluate(text)...
+...numexpr.evaluate(single line mathematical expression that solves the problem)...
 ```output
 ${{Output of running the code}}
 ```
 Answer: ${{Answer}}
 
-Begin.
+Here are some examples:
 
 Question: What is 37593 * 67?
 ```text
@@ -36,6 +39,8 @@ Question: 37593^(1/5)
 ```
 Answer: 8.222831614237718
 
+Begain.
+
 Question: {question}
 """
 
@@ -50,15 +55,21 @@ class Calulator:
         prompts = self.template.format(question=input)
         completion = self.client.create(model=self.model_name, messages=[{"role": "user", "content": prompts}])
         llm_output = completion.choices[0].message.content
-        print(llm_output)  
-        pattern = r'numexpr\.evaluate\("([^"]+)"\)'
-        matches = re.findall(pattern, llm_output)
-        expression = matches[0].strip()
-        
+      
         try:
-            output = str(numexpr.evaluate(expression, global_dict={}))
+            pattern = r'numexpr\.evaluate\("([^"]+)"\)'
+            matches = re.findall(pattern, llm_output)
+            expression = matches[0].strip()
+            
+            local_dict = {"pi": math.pi, "e": math.e}
+            output = str(
+                numexpr.evaluate(
+                    expression, 
+                    global_dict={},
+                    local_dict=local_dict)
+            )
         except Exception as e:
-            error_msg = f'"{input}" raised error: {e}. Please try again with a simple question, such as "What is 37 * 67?"'
+            error_msg = f'"{input}" raised error: {e}. Please try again with a valid numerical expression"'
             return error_msg
             
         return re.sub(r"^\[|\]$", "", output)
