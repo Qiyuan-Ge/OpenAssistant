5d238f4be4a8d1db667b79ec5606d26b419f3fdb
Qiyuan-Ge <53368178+Qiyuan-Ge@users.noreply.github.com>
2023-09-01 23:48:38 +0800

    Update 3_customize.py

--- pages/3_customize.py
+++ pages/3_customize.py
@@ -53,7 +53,7 @@ with tab3:
     """)
     st.session_state.generate_params['max_tokens'] = st.slider(
         'Max New Tokens', 
-        min_value=1, 
+        min_value=32, 
         max_value=4096, 
         value=st.session_state.generate_params['max_tokens'], 
         label_visibility="collapsed",
