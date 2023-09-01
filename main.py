ec2bc1273e01dba9da3b5485bd6ad5321a452a44
Qiyuan-Ge <53368178+Qiyuan-Ge@users.noreply.github.com>
2023-09-01 23:30:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,9 @@ def init_server():
         st.session_state.server_api_key = "EMPTY"
     if "server_api_base" not in st.session_state:
         st.session_state.server_api_base = "https://u31193-92ae-b10f516b.neimeng.seetacloud.com:6443/v1" #"https://api.openai.com/v1"
-
+    if "generate_params" not in st.session_state:
+        st.session_state.generate_params = {'max_tokens':4096, 'temperature':0.9, 'top_p':0.6}
+        
 
 init_server()
 
