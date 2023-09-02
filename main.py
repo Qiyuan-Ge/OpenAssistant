e3b978fd7ab0265358f631a367e889f8fd29f14d
Qiyuan-Ge <53368178+Qiyuan-Ge@users.noreply.github.com>
2023-09-02 19:02:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -186,10 +186,10 @@ def main():
             st.button(f"a. {predictions[0]}", key='b1', on_click=click_add_message, kwargs={'message':predictions[0]})
             st.button(f"b. {predictions[1]}", key='b2', on_click=click_add_message, kwargs={'message':predictions[1]})
         
-        st.button("try again", key='b3', on_click=try_again)
-        st.button("clear conversation", key='b4', on_click=clear_messages)
+        st.button("try again", key='b3', on_click=try_again)  
     if len(messages) > 0:
-        st.button("go back", key='b5', on_click=go_back)
+        st.button("go back", key='b4', on_click=go_back)
+        st.button("clear conversation", key='b5', on_click=clear_messages)
 
         
 if __name__ == "__main__":
