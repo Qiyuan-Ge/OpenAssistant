8912219ef902b9e8650c1bec9c577e5ffe963207
Qiyuan-Ge <53368178+Qiyuan-Ge@users.noreply.github.com>
2023-09-02 17:53:11 +0800

    Update watsen.py

--- open_assistant/watsen.py
+++ open_assistant/watsen.py
@@ -4,7 +4,7 @@ from tqdm.auto import tqdm
 from typing import List, Dict
     
       
-prompt_template = """Question: {instruction}
+prompt_template_1 = """Question: {instruction}
 {response}"""
     
 class SimCaseSearch:
@@ -13,7 +13,7 @@ class SimCaseSearch:
         self.model_name = model_name
         self.client = openai.Embedding
         self.embeddings = None
-        self.template = prompt_template
+        self.template = prompt_template_1
 
     def create_embeddings(self):
         embeddings = []
@@ -34,3 +34,46 @@ class SimCaseSearch:
         rec = self.data[idx]
         
         return self.template.format(instruction=rec['instruction'], response=rec['response'])
+
+
+prompt_template_2 = """You are provided with a conversation history between an AI assistant and a user. Based on the context of the conversation, please predict the three most probable questions or requests the user is likely to make next.
+
+Previous conversation history:
+{conversation}
+
+Please respond in the following format:
+1. text
+2. text
+3. text
+
+Your predictions:
+""" 
+
+class ConversationMimic:
+    def __init__(self, model_name: str = "gpt-3.5-turbo", roles: dict = {'user':'user', 'assistant':'assistant'}):
+        self.client = openai.ChatCompletion
+        self.model_name = model_name
+        self.roles = roles
+        self.template = prompt_template_2
+        
+    def __call__(self, messages: List[dict], context_window_size: int = 4):
+        conv_prompt = self.convert_messages_to_conversation(messages, context_window_size)
+        prompt = self.template.format(conversation=conv_prompt)
+        llm_output = self.llm(prompt)
+        
+        pattern = r"\d+\.\s*(.+)"
+        predictions = re.findall(pattern, llm_output)
+        
+        return predictions
+    
+    def convert_messages_to_conversation(self, messages: List[dict], context_window_size: int = 4):
+        conv_prompt = ""
+        for i, message in enumerate(messages[::-1]):
+            if i == context_window_size:
+                break
+            if message["role"] == "assistant":
+                conv_prompt = f"{self.roles['assistant']}: {message['content']}\n" + conv_prompt
+            elif message["role"] == "user":
+                conv_prompt = f"{self.roles['user']}: {message['content']}\n" + conv_prompt
+                
+        return conv_prompt
