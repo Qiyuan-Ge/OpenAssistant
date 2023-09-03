76cd90b8ca3b0d7a0e56bd4ff60852087d6d299d
Qiyuan-Ge <53368178+Qiyuan-Ge@users.noreply.github.com>
2023-09-03 10:39:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -141,6 +141,13 @@ def main():
     avatar_user = None
     avatar_assistant = None
     messages = init_messages(avatar_user=avatar_user, avatar_assistant=avatar_assistant)
+
+    if len(messages) == 0:
+        example1 = "Who are you?"
+        example2 = "Give me a summary of this web page: https://github.com/Qiyuan-Ge/OpenAssistant"
+        st.button(f"💬{example1}", key='b6', on_click=click_add_message, kwargs={'message':example1})
+        st.button(f"💬{example2}", key='b7', on_click=click_add_message, kwargs={'message':example2})
+        
     
     if prompt := st.chat_input("Shift + Enter 换行, Enter 发送"):
         with st.chat_message("user", avatar=avatar_user):
