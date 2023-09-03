da5e31d7a4d821f711b8792978d230e9569a0970
Qiyuan-Ge <53368178+Qiyuan-Ge@users.noreply.github.com>
2023-09-03 11:40:06 +0800

    Update __init__.py

--- open_assistant/tools/__init__.py
+++ open_assistant/tools/__init__.py
@@ -6,6 +6,7 @@ from .google_search import GoogleSearch
 from .weather import OpenWeatherMapAPIWrapper
 from .browse_website import BrowseWebsiteWithQuestion
 from .llm_math import Calulator
+from .translator import Translator
 
             
 def load_tools(tool_names: List[str], model_name="gpt-3.5-turbo", embedding_model_name="text-embedding-ada-002") -> List[Tool]:
@@ -52,6 +53,17 @@ def load_tools(tool_names: List[str], model_name="gpt-3.5-turbo", embedding_mode
                     func=tool_func.run,
                     description='language model that interprets a prompt and executes python code to do math, args: {"input": "math expression"}',
                 )
+                tools.append(tool)
+            except:
+                st.info(f"Failed to add {tool_name} to tool list", icon="ℹ️")
+        elif tool_name == "Translator":
+            try:
+                tool_func = Translator(model_name=model_name)
+                tool = StructuredTool.from_function(
+                    name="Translator",
+                    func=tool_func.run,
+                )
+                tool.description = 'translate text to another language, args: {"text": "text", "language":"language"}'
                 tools.append(tool)
             except:
                 st.info(f"Failed to add {tool_name} to tool list", icon="ℹ️")
