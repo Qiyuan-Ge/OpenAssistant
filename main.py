492ef4ee19d41f34c3faaa1551296b3285ba666a
Qiyuan-Ge <53368178+Qiyuan-Ge@users.noreply.github.com>
2023-09-03 13:05:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -149,7 +149,7 @@ def main():
 
     if len(messages) == 0:
         with st.container():
-            col1, col2, col3 = st.columns([1, 1.5, 3])
+            col1, col2, col3 = st.columns([2, 2, 3])
             with col1:
                 example1 = "Who are you?"
                 st.button(f"💬{example1}", key='b_r1', on_click=click_add_message, kwargs={'message':example1})
