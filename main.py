db232c777befae8d6170acf9c68a9268f71fb7fa
Qiyuan-Ge <53368178+Qiyuan-Ge@users.noreply.github.com>
2023-09-03 14:38:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -236,7 +236,7 @@ def main():
             with col2:
                 lang = st.text_input(label="Language", key='lang', value=st.session_state.translation_lang, max_chars=20)
             with col3:
-                st.button(Translate, key='b_trans', on_click=translate_func, kwargs={'text':text, 'language':lang})
+                st.button("Translate", key='b_trans', on_click=translate_func, kwargs={'text':text, 'language':lang})
             st.session_state.translation = text
             st.session_state.translation_lang = lang
             st.write(res)
